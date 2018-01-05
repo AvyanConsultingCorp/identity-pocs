bb149e10edb2bc4ecc9f6988bb3b4132e8ac02d5
Shraddha <shraddha@avyanconsulting.com>
2018-01-05 16:01:14 +0530

    Working web app template and powershell script

--- ScenarioOne/Platform/IdentityScenarioOne.Webapp/IdentityScenarioOne.Webapp/Controllers/HomeController.cs
+++ ScenarioOne/Platform/IdentityScenarioOne.Webapp/IdentityScenarioOne.Webapp/Controllers/HomeController.cs
@@ -1,15 +1,19 @@
-﻿using System;
+﻿using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using System;
 using System.Collections.Generic;
 using System.IdentityModel.Tokens;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using System.Configuration;
 
 namespace IdentityScenarioOne.Webapp.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        static AuthenticationResult result;
+        public async Task<ActionResult> Index()
         {
             if (!Request.IsAuthenticated)
             {
@@ -25,11 +29,31 @@ namespace IdentityScenarioOne.Webapp.Controllers
                 ViewBag.IpAdress = userClaims?.FindFirst("ipaddr")?.Value;
                 ViewBag.Email = userClaims?.FindFirst(System.IdentityModel.Claims.ClaimTypes.Email)?.Value;
                 ViewBag.Username = userClaims?.FindFirst(System.Security.Claims.ClaimTypes.Name)?.Value;
+                string adAppClientId = ConfigurationManager.AppSettings["adAppClientId"];
+                string authority = ConfigurationManager.AppSettings["tenantDomain"];
+                string adAppClientPassword = ConfigurationManager.AppSettings["adAppClientPassword"];
+                var clientCredential = new ClientCredential(adAppClientId, adAppClientPassword);
+
+                var authContext = new Microsoft.IdentityModel.Clients.ActiveDirectory.AuthenticationContext(authority, false);
+                result = await authContext.AcquireTokenAsync("https://graph.windows.net", clientCredential);
+                ViewBag.AuthToken = result.AccessToken;
 
                 // TenantId is the unique Tenant Id - which represents an organization in Azure AD
                 //ViewBag.TenantId = userClaims?.FindFirst("http://schemas.microsoft.com/identity/claims/tenantid")?.Value;
                 return View();
             }
+        }
+
+        public ActionResult signout()
+        {
+            string[] myCookies = Request.Cookies.AllKeys;
+            foreach (string cookie in myCookies)
+            {
+                Response.Cookies[cookie].Expires = DateTime.Now.AddDays(-1);
+            }
+            string webAppURL = ConfigurationManager.AppSettings["webAppURL"];
+            string redirectURI = "https://login.microsoftonline.com/46d804b6-210b-4a4a-9304-83b93e71784d/oauth2/logout?post_logout_redirect_uri=" + webAppURL;
+            return Redirect(redirectURI);
         }
 
         public ActionResult About()
