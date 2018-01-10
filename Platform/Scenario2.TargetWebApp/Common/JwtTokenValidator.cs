35b603c17ee8e7691a7c46b90227d40cecebf555
unmesh <unmeshv@avyanconsulting.com>
2018-01-10 15:19:39 +0530

    updated changes

---
+++ Platform/Scenario2.TargetWebApp/Common/JwtTokenValidator.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.IdentityModel.Protocols;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using System.Threading;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System;
+using Scenario2.TargetWebApp.Models;
+using System.Collections.Generic;
+
+namespace Scenario2.TargetWebApp.Common
+{
+    public static class JwtTokenValidator
+    {
+
+        
+        public static bool Validate(string authIssuer,string authAudience, string token,ref TokenData tokenData)
+        {
+            string stsDiscoveryEndpoint = "https://login.microsoftonline.com/common/v2.0/.well-known/openid-configuration";
+
+
+            ConfigurationManager<OpenIdConnectConfiguration> configManager
+                = new ConfigurationManager<OpenIdConnectConfiguration>(stsDiscoveryEndpoint, 
+                new OpenIdConnectConfigurationRetriever());
+            OpenIdConnectConfiguration config = configManager.GetConfigurationAsync().Result;
+            TokenValidationParameters validationParameters = new TokenValidationParameters
+            {
+                ValidateAudience = true,
+                ValidateIssuer = false,
+                ValidateLifetime = false,
+                ValidAudience = authAudience,
+                ValidIssuer = authIssuer,
+                IssuerSigningKeys = config.SigningKeys
+            };
+
+            JwtSecurityTokenHandler tokendHandler = new JwtSecurityTokenHandler();
+            SecurityToken jwt;
+
+            try
+            {
+                var result = tokendHandler.ValidateToken(token, validationParameters, out jwt);
+
+                var data = new Dictionary<string, string>();
+
+                foreach(var claim in result.Claims)
+                {
+                    data.Add(claim.Type, claim.Value);
+                }
+
+                tokenData.Issuer = jwt.Issuer;
+                tokenData.ValidFrom = jwt.ValidFrom;
+                tokenData.ValidTo = jwt.ValidTo;
+                return true;
+            }
+            catch(Exception ex)
+            {
+                return false;
+            }
+
+           
+        }
+    }
+}
